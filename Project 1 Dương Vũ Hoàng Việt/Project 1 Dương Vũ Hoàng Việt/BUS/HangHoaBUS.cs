9ac3e0d3e5c9bec88807a6da8e7c05fe1153098d
Hoàng Việt <70848809+VietCongSoldier@users.noreply.github.com>
2020-11-05 02:53:10 +0700

    Do An 1
    update 3

--- Project 1 Dương Vũ Hoàng Việt/Project 1 Dương Vũ Hoàng Việt/BUS/HangHoaBUS.cs
+++ Project 1 Dương Vũ Hoàng Việt/Project 1 Dương Vũ Hoàng Việt/BUS/HangHoaBUS.cs
@@ -6,5 +6,22 @@ namespace Project_1_Dương_Vũ_Hoàng_Việt.DAL
 {
     class HangHoaBUS
     {
+        private HangHoaDAL hanghoaDAL = new HangHoaDAL();
+        public List<string> DanhSachSP()
+        {
+            return hanghoaDAL.DanhSachSP();
+        }
+        public void ThemSP(string masp, string tensp, string donvitinh, string phuongthuctt, int dongia, int soluong, int thanhtien)
+        {
+            hanghoaDAL.Themsp(masp,tensp,donvitinh,phuongthuctt,dongia,soluong,thanhtien);
+        }
+        public void Suasp(string masp, string tensp, string donvitinh, string phuongthuctt, int dongia, int soluong, int thanhtien)
+        {
+            hanghoaDAL.Suasp(masp,tensp,donvitinh,phuongthuctt,dongia,soluong,thanhtien);
+        }
+        public void XoaSP(string masp)
+        {
+            hanghoaDAL.Xoasp(masp);
+        }
     }
 }
