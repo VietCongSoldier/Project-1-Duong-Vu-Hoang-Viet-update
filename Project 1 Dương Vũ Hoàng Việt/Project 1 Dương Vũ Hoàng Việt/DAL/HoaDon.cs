ca7b824dd8e975f748a4c36fe71ff765315ec72c
Hoàng Việt <70848809+VietCongSoldier@users.noreply.github.com>
2020-11-05 02:30:58 +0700

    Do An 1
    update code

---
+++ Project 1 Dương Vũ Hoàng Việt/Project 1 Dương Vũ Hoàng Việt/DAL/HoaDon.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Project_1_Dương_Vũ_Hoàng_Việt.DAL
+{
+    class HoaDon
+    {
+        private StreamReader SR;
+        private StreamWriter SW;
+        private string filename = "hanghoa.txt";
+        public HoaDon()
+        {
+
+        }
+        public void HienThiHangHoa()
+        {
+            Console.Clear(); Console.Beep();
+            SR = new StreamReader(filename);
+            string docfile;
+            Console.SetCursorPosition(1, 0);
+            Console.WriteLine("Mã SP");
+            Console.SetCursorPosition(10, 0);
+            Console.WriteLine("Tên SP");
+            Console.SetCursorPosition(27, 0);
+            Console.WriteLine("Đơn giá");
+            Console.SetCursorPosition(40, 0);
+            Console.WriteLine("Số Lượng");
+            Console.SetCursorPosition(50, 0);
+            Console.WriteLine("Đơn vị tính");
+            Console.SetCursorPosition(64, 0);
+            Console.WriteLine("Thành tiền(VNĐ)");
+            Console.SetCursorPosition(81, 0);
+            Console.WriteLine("Phương thức thanh toán");
+            do
+            {
+                docfile = SR.ReadLine();
+                if (docfile == null)
+                {
+                    break;
+                }
+                try
+                {
+                    string[] plit;
+                    plit = docfile.Split("|");
+                    int dong = int.Parse(plit[0]);
+                    Console.SetCursorPosition(1, dong);
+                    Console.WriteLine(plit[0]);
+                    Console.SetCursorPosition(10, dong);
+                    Console.WriteLine(plit[1]);
+                    Console.SetCursorPosition(27, dong);
+                    Console.WriteLine(plit[2]);
+                    Console.SetCursorPosition(40, dong);
+                    Console.WriteLine(plit[3]);
+                    Console.SetCursorPosition(50, dong);
+                    Console.WriteLine(plit[4]);
+                    Console.SetCursorPosition(64, dong);
+                    Console.WriteLine(plit[5]);
+                    Console.SetCursorPosition(81, dong);
+                    Console.WriteLine(plit[6]);
+                }
+                catch (Exception e)
+                {
+                }
+
+            }
+            while (docfile != null);
+            SR.Close();
+        }
+        public void Themsp()
+        {
+            Console.Clear();
+            SW = new StreamWriter(filename, true);
+            string tensp, masp, donvitinh, phuongthuctt;
+            int dongia, soluong;
+            do
+            {
+                Console.SetCursorPosition(45, 8); Console.Beep();
+                Console.Write("Nhập Mã SP:"); masp = Console.ReadLine();
+            } while (masp == "");
+            do
+            {
+                Console.SetCursorPosition(45, 9); Console.Beep();
+                Console.Write("Nhập Tên SP:"); tensp = Console.ReadLine();
+            } while (tensp == "");
+            Console.SetCursorPosition(45, 10); Console.Beep();
+            Console.Write("Đơn giá:"); dongia = int.Parse(Console.ReadLine());
+            Console.SetCursorPosition(45, 11); Console.Beep();
+            Console.Write("Số lượng:"); soluong = int.Parse(Console.ReadLine());
+            Console.SetCursorPosition(45, 12); Console.Beep();
+            Console.Write("Đơn vị tính:"); donvitinh = Console.ReadLine();
+            Console.SetCursorPosition(45, 13); Console.Beep();
+            Console.Write("Phương thức thanh toán:"); phuongthuctt = Console.ReadLine();
+            int thanhtien = dongia * soluong;
+            Console.SetCursorPosition(45, 14); Console.Beep();
+            Console.WriteLine($"Thành tiền:{thanhtien} (VNĐ)");
+            Console.SetCursorPosition(45, 15); Console.Beep();
+            Console.WriteLine("Đã thêm sản phẩm vào kho !");
+            SW.WriteLine($"{masp}|{tensp}|{dongia}|{soluong}|{donvitinh}|{thanhtien}|{phuongthuctt}\n");
+            SW.Close();
+        }
+        public void Suasp()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(43, 9); Console.Beep();
+            Console.Write("Nhập Mã SP cần sửa:"); string masp = Console.ReadLine();
+            Console.SetCursorPosition(43, 10); Console.Beep();
+            Console.Write("Nhập Mã SP mới:"); string tensp = Console.ReadLine();
+            Console.SetCursorPosition(43, 11); Console.Beep();
+            Console.Write("Nhập Tên SP mới:"); string maspmoi = Console.ReadLine();
+            Console.SetCursorPosition(43, 12); Console.Beep();
+            Console.Write("Đơn giá:"); int dongia = int.Parse(Console.ReadLine());
+            Console.SetCursorPosition(43, 13); Console.Beep();
+            Console.Write("Số lượng:"); int soluong = int.Parse(Console.ReadLine());
+            Console.SetCursorPosition(43, 14); Console.Beep();
+            Console.Write("Đơn vị tính:"); string donvitinh = Console.ReadLine();
+            Console.SetCursorPosition(43, 15); Console.Beep();
+            Console.Write("Phương thức thanh toán:"); string phuongthuctt = Console.ReadLine();
+            Console.SetCursorPosition(43, 16); Console.Beep();
+            Console.WriteLine("Sản phẩm đã được sửa thành công !");
+            int thanhtien = dongia * soluong;
+            string temp = "";
+            SR = new StreamReader(filename);
+            string read;
+            while ((read = SR.ReadLine()) != null)
+            {
+                if (read == null) break;
+                string[] dlhanghoa = read.Split('|');
+                if (dlhanghoa[0] == masp)
+                {
+                    temp += maspmoi + '|' + tensp + '|' + dongia + '|' + soluong + '|' + donvitinh + '|' + thanhtien + '|' + phuongthuctt;
+                }
+                else
+                {
+                    temp += dlhanghoa[0] + '|' + dlhanghoa[1] + '|' + dlhanghoa[2] + '|' + dlhanghoa[3] + '|' + dlhanghoa[4] + '|' + dlhanghoa[5] + '|' + dlhanghoa[6];
+                }
+            }
+            SR.Close();
+            SW = new StreamWriter(filename);
+            SW.Write(temp);
+            SW.Close();
+        }
+        public void Xoasp()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(43, 9); Console.Beep();
+            Console.Write("Nhập Mã SP cần xóa:"); string masp = Console.ReadLine();
+            string temp = "";
+            SR = new StreamReader(filename);
+            string read;
+            while ((read = SR.ReadLine()) != null)
+            {
+                string[] dlhanghoa = read.Split('|');
+                if (dlhanghoa[0] == masp)
+                {
+                    continue;
+                }
+                else
+                {
+                    temp += dlhanghoa[0] + '|' + dlhanghoa[1] + '|' + dlhanghoa[2] + '|' + dlhanghoa[3] + '|' + dlhanghoa[4] + '|' + dlhanghoa[5] + '\n';
+                }
+            }
+            Console.SetCursorPosition(43, 11); Console.Beep();
+            Console.WriteLine("Đã xóa sản phẩm !");
+            SR.Close();
+            SW = new StreamWriter(filename);
+            SW.Write(temp);
+            SW.Close();
+        }
+        public void Timkiemsp()
+        {
+
+        }
+    }
+}
---
+++ Project 1 Dương Vũ Hoàng Việt/Project 1 Dương Vũ Hoàng Việt/GUI/GUI_Chinh.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Text;
+
+namespace Project_1_Dương_Vũ_Hoàng_Việt
+{
+    class GUI_Chinh
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            MENUCHINH();
+        }
+        static void MENUCHINH()
+        {
+            Console.SetCursorPosition(40, 1); Console.WriteLine("CHƯƠNG TRÌNH QUẢN LÝ KHO NHÀ HÀNG");
+            Console.SetCursorPosition(25,  8); Console.WriteLine("______________________________________________________________________");
+            Console.SetCursorPosition(25,  9); Console.WriteLine("|                              MENU                                  |");
+            Console.SetCursorPosition(25, 10); Console.WriteLine("|____________________________________________________________________|");
+            Console.SetCursorPosition(25, 11); Console.WriteLine("|              1. Quản lý danh sách sản phẩm trong kho.              |");
+            Console.SetCursorPosition(25, 12); Console.WriteLine("|              2. Quản lý hóa đơn nhập xuất kho.                     |");
+            Console.SetCursorPosition(25, 13); Console.WriteLine("|              3.     |");
+            Console.SetCursorPosition(25, 14); Console.WriteLine("|              4. Hiển thị toàn bộ sản phẩm trong kho.               |");
+            Console.SetCursorPosition(25, 15); Console.WriteLine("|              5. Thoát.                                             |");
+            Console.SetCursorPosition(25, 16); Console.WriteLine("|                                                                    |");
+            Console.SetCursorPosition(25, 17); Console.WriteLine("|                                                                    |");
+            Console.SetCursorPosition(25, 18); Console.WriteLine("|                                                                    |");
+            Console.SetCursorPosition(25, 19); Console.WriteLine("|____________________________________________________________________|");
+            string chon;
+                Console.SetCursorPosition(48, 18); Console.Write("Chọn công cụ số:");Console.Beep();
+                chon = Console.ReadLine();
+            Console.Clear();
+            if (chon == "1")
+            {
+                MENUHangHoa();
+            }
+            else if (chon == "2")
+            {
+                Console.Beep();
+                HangHoa HH = new HangHoa();
+                
+                Console.ReadKey();
+            }
+            else if (chon == "3")
+            {
+                Console.Beep();
+                HangHoa HH = new HangHoa();
+                
+                Console.ReadKey();
+            }
+            else if (chon == "4")
+            {
+                Console.Beep();
+                HangHoa HH = new HangHoa();
+                HH.HienThiHangHoa();
+                Console.ReadKey();
+            }
+            else if (chon == "5")
+            {
+                Console.Beep();
+                return;
+            }
+            Console.Clear();
+            MENUCHINH();
+        }
+        static void MENUHangHoa()
+        {
+            Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition(25,  8); Console.WriteLine(" ____________________________________________________________________");
+            Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition(25,  9); Console.WriteLine("|                    Quản Lý Danh Sách Sản Phẩm                      |");
+            Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition(25, 10); Console.WriteLine("|____________________________________________________________________|");
+            Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition(25, 11); Console.WriteLine("|                1. Hiển thị toàn bộ sản phẩm trong kho.             |");
+            Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition(25, 12); Console.WriteLine("|                2. Thêm sản phẩm vào kho.                           |");
+            Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition(25, 13); Console.WriteLine("|                3. Sửa sản phẩm trong kho.                          |");
+            Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition(25, 14); Console.WriteLine("|                4. Xóa sản phẩm trong kho.                          |");
+            Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition(25, 15); Console.WriteLine("|                5. Tìm Kiếm sản phẩm trong kho.                     |");
+            Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition(25, 16); Console.WriteLine("|                6. Quay lại.                                        |");
+            Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition(25, 17); Console.WriteLine("|                                                                    |");
+            Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition(25, 18); Console.WriteLine("|____________________________________________________________________|");
+            Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition(48, 17); Console.Write("Chọn công cụ số:"); Console.Beep();
+            string chon = Console.ReadLine();
+            if (chon == "1")
+            {
+                HangHoa HH = new HangHoa();
+                HH.HienThiHangHoa();
+                Console.ReadKey();
+            }
+            else if (chon == "2")
+            {
+                HangHoa HH = new HangHoa();
+                HH.Themsp();
+                Console.ReadKey();
+            }
+            else if (chon == "3")
+            {
+                HangHoa HH = new HangHoa();
+                HH.Suasp();
+                Console.ReadKey();
+            }
+            else if (chon == "4")
+            {
+                HangHoa HH = new HangHoa();
+                HH.Xoasp();
+                Console.ReadKey();
+            }
+            else if (chon == "5")
+            {
+                HangHoa HH = new HangHoa();
+                HH.Timkiemsp();
+                Console.ReadKey();
+            }
+            else if (chon == "6")
+            {
+                Console.Beep();
+                return;
+            }
+            Console.Clear();
+            MENUHangHoa();
+        }
+    }
+}
